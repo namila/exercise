b9787298745bad574af2806a46bef321b01122c6
Namila Perera <NamilaP@99x.lk>
2019-12-06 21:01:55 +0530

    Task 2 done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const Task1  = require('./task1');
+const Task2 = require('./task2');
 
-const task1 = new Task1();
-task1.printNumbers();
+// const task1 = new Task1();
+// task1.printNumbers();
+
+const task2 = new Task2();
+task2.printFizzBuzz();
 
 
